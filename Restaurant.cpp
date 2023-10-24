bfb8a0ab73fe45289bf2ef09a810b270ad1337b5
loc-dev <nguyentheloc310101@gmail.com>
2023-10-24 23:33:31 +0700

    fix(add customer): add left and right constructor fix

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -32,10 +32,94 @@ public:
 		{
 			filled++;
 		}
-		void addCustomerQueue(customer *Customer);
+		void decreaseFilled()
+		{
+			filled--;
+		}
+		void removeCustomer(int n)
+		{
+			while (n > 0)
+			{
+				customer *temp = first;
+				first = first->next;
+				delete temp;
+				n--;
+				filled--;
+			}
+			last = first;
+			if (first == NULL)
+			{
+				last = NULL;
+			}
+		}
+		void deleteQueue()
+		{
+			customer *temp = first;
+			int count = filled;
+			while (count != 0)
+			{
+				customer *tmpForDelete = temp;
+				temp = temp->next;
+				delete tmpForDelete;
+			}
+			last = NULL;
+			first = NULL;
+			filled = 0;
+		}
+		customer *getCustomerFromQueue(int n)
+		{
+
+			// customer *currentDelete = first;
+			customer *returnCurrent = first;
+			int count = 0;
+			while (count != n)
+			{
+				returnCurrent = returnCurrent->next;
+				count++;
+			}
+			return returnCurrent;
+		}
 		void getCustomerInQueue();
 		void checkSizeQueue();
 		void searchCustomerInQueue(string);
+		void addCustomerQueue(customer *Customer)
+		{
+			if (filled < queueSize)
+			{
+				if (filled < queueSize)
+				{
+					if (first == nullptr)
+					{
+						Customer->next = nullptr;
+						first = Customer;
+						last = Customer;
+					}
+					else
+					{
+						last->next = Customer;
+						last = Customer;
+						last->next = nullptr;
+						Customer->next = nullptr;
+					}
+					increaseFilled();
+				}
+			}
+		}
+		void printQueue()
+		{
+			customer *current = first;
+			int count = filled;
+			cout << "Queue printing - filled: " << filled << endl;
+			while (count > 0)
+			{
+				// Assuming you have a toString() method in the 'customer' class to display customer information
+				cout << "Queue printing: " << current->name << endl;
+				current = current->next;
+				count--;
+			}
+			// cout << last->name << endl;
+			return;
+		}
 	};
 
 private:
@@ -44,7 +128,7 @@ private:
 public:
 	customer *currentChange;
 	Queue *customerQueue;
-	Queue *CustomerOrder;
+	Queue *customerOrder;
 	customer *head; // for travel
 	int occupied;
 	imp_res()
@@ -53,60 +137,59 @@ public:
 		head = nullptr;
 		currentChange = nullptr;
 		customerQueue = new Queue();
-		CustomerOrder = new Queue();
+		customerOrder = new Queue();
 	};
 
 	void RED(string name, int energy)
 	{
-		cout << name << " " << energy << endl;
+		// cout << name << " " << energy << endl;
 		customerQueue->updateSize(MAXSIZE);
-		CustomerOrder->updateSize(MAXSIZE);
+		customerOrder->updateSize(MAXSIZE);
 
-		if (energy > 0 || energy < 0)
+		if (energy != 0)
 		{
-			customer *newCustomer = new customer(name, energy, nullptr, nullptr);
-			if (isNameExist(newCustomer->name) == false)
+
+			if (isNameExist(name) == false)
 			{
-				// CustomerOrder->addCustomerQueue(newCustomer);
+				customer *newCustomer = new customer(name, energy, nullptr, nullptr);
+				customerOrder->addCustomerQueue(newCustomer);
 				if (head == nullptr || occupied == 0)
 				{
 					head = newCustomer;
 					currentChange = newCustomer;
-					newCustomer->next = head;
 					occupied++;
 					return;
 				}
-
-				if (occupied < (MAXSIZE / 2))
+				else if (occupied < (MAXSIZE / 2))
 				{
-					// if energy new > current energy  -> add right
-					if (currentChange->energy < newCustomer->energy)
+					if (currentChange->energy <= newCustomer->energy)
 					{
-						addRight(newCustomer);
+
+						addRight(name, energy);
 					}
-					// if energy new < current energy -> add left
-					else if (currentChange->energy < newCustomer->energy)
+					else if (currentChange->energy > newCustomer->energy)
+
 					{
-						addLeft(newCustomer);
+						addLeft(name, energy);
 					}
 				}
-				else if (occupied >= (MAXSIZE / 2) && (occupied < MAXSIZE))
+				else if ((occupied >= (MAXSIZE / 2)) && (occupied < MAXSIZE))
 				{
-					// set flag > 0 or < 0, and move currentChange to that flag postion
-					int flag = countRes(newCustomer->energy);
+					int flag = countRes(energy);
 					if (flag < 0)
 					{
-						addLeft(newCustomer);
+						addLeft(name, energy);
 					}
 					else if (flag > 0)
 					{
-						addRight(newCustomer);
+						addRight(name, energy);
 					}
 				}
 				else if (occupied == MAXSIZE)
 				{
 					if (customerQueue->getFilled() == MAXSIZE)
 						return;
+					customerQueue->addCustomerQueue(newCustomer);
 				}
 			}
 			else
@@ -118,30 +201,66 @@ public:
 		{
 			return;
 		}
+		// CustomerOrder->printQueue();
 	}
 	void BLUE(int num)
 	{
-		cout << "blue " << num << endl;
+		cout << "occupied: " << occupied << " "
+			 << "num:" << num << endl;
+		if (num >= occupied || num >= MAXSIZE)
+		{
+			deleteAllCustomer();
+			return;
+		}
+
+		int count = 0;
+		while (count != num)
+		{
+			customer *customerDelete = customerOrder->getCustomerFromQueue(count);
+			findAndDeleteCustomer(customerDelete->name);
+			count++;
+		}
 	}
 	void PURPLE()
 	{
-		cout << "purple" << endl;
+		// cout << "purple" << endl;
 	}
 	void REVERSAL()
 	{
-		cout << "reversal" << endl;
+		// cout << "reversal" << endl;
 	}
 	void UNLIMITED_VOID()
 	{
-		cout << "unlimited_void" << endl;
+		// cout << "unlimited_void" << endl;
 	}
 	void DOMAIN_EXPANSION()
 	{
-		cout << "domain_expansion" << endl;
+		// cout << "domain_expansion" << endl;
 	}
 	void LIGHT(int num)
 	{
-		cout << "light " << num << endl;
+		if (num > 0)
+		{
+			customer *temp = head;
+			int count = occupied;
+			while (count > 0)
+			{
+				temp->print();
+				temp = temp->next;
+				count--;
+			}
+		}
+		else if (num < 0)
+		{
+			customer *temp = head;
+			int count = occupied;
+			while (count > 0)
+			{
+				temp->print();
+				temp = temp->prev;
+				count--;
+			}
+		}
 	}
 	int countRes(int energy)
 	{
@@ -180,13 +299,46 @@ public:
 		}
 		return false;
 	}
-
-	void addLeft(customer *newCustomer)
+	void checkSize()
+	{
+		int count = 0;
+		customer *ptr = head->next;
+		while (ptr != head)
+		{
+			cout << "size of table occupied: " << count << endl;
+			count++;
+		}
+	}
+	void addLeft(string name, int energy)
 	{
 		if (occupied == 1)
 		{
-			newCustomer->next = head;
-			newCustomer->prev = head;
+			customer *newCustomer = new customer(name, energy, currentChange, currentChange);
+
+			currentChange->next = newCustomer;
+			currentChange->prev = newCustomer;
+
+			currentChange = newCustomer;
+		}
+		else
+		{
+			customer *tempCurrentNext = currentChange->next;
+			// customer *temp = curr->prev;
+			customer *newCustomer = new customer(name, energy, tempCurrentNext, currentChange);
+			tempCurrentNext->next = newCustomer;
+			currentChange->prev = newCustomer;
+
+			currentChange = newCustomer;
+		}
+		occupied++;
+	}
+
+	void addRight(string name, int energy)
+	{
+
+		if (occupied == 1)
+		{
+			customer *newCustomer = new customer(name, energy, currentChange, currentChange);
 
 			currentChange->next = newCustomer;
 			currentChange->prev = newCustomer;
@@ -197,59 +349,66 @@ public:
 		else
 		{
 			customer *tempCurrentNext = currentChange->next;
+			customer *newCustomer = new customer(name, energy, currentChange, tempCurrentNext);
+
 			currentChange->next = newCustomer;
-			newCustomer->next = tempCurrentNext;
-
 			tempCurrentNext->prev = newCustomer;
-			newCustomer->prev = currentChange;
-
 			currentChange = newCustomer;
 		}
+
 		occupied++;
 	}
-
-	void addRight(customer *newCustomer)
+	void deleteAllCustomer()
 	{
-		if (occupied == 1)
+		if (head == NULL)
 		{
-			currentChange->next = newCustomer;
-			currentChange->prev = newCustomer;
-
-			newCustomer->prev = head;
-			newCustomer->next = head;
-
-			currentChange = newCustomer;
+			return;
 		}
-		else
+		customer *temp, *current;
+		current = head->next;
+		while (current != head)
 		{
-			customer *tempCurrentNext = currentChange->next;
-			//-> <-
-			currentChange->next = newCustomer;
-			newCustomer->next = tempCurrentNext;
-
-			tempCurrentNext->prev = newCustomer;
-			newCustomer->prev = currentChange;
-
-			// update latest change
-			currentChange = newCustomer;
+			temp = current->next;
+			// free(current);
+			current = temp;
 		}
-		occupied++;
+		// customerQueue->deleteQueue();
+		// customerOrder->deleteQueue();
+
+		head = NULL;
+		currentChange = NULL;
+		occupied = 0;
+		return;
 	}
 
-	// void addCustomerQueue(customer *Customer)
-	// {
-	// 	if (customerQueue->getFilled() == 0)
-	// 	{
-	// 		customerQueue->first = Customer;
-	// 		customerQueue->last = Customer;
-	// 	}
-	// 	else
-	// 	{
-	// 		customerQueue->last->next = Customer;
-	// 		Customer->prev = customerQueue->last;
-	// 		customerQueue->last = Customer;
-	// 	}
+	void findAndDeleteCustomer(string name)
+	{
+		customer *tmp = head;
+		int index = occupied;
+		while (tmp->name != name)
+		{
+			tmp = tmp->next;
+		}
+		customer *prevTemp = tmp->prev;
+		customer *nextTemp = tmp->next;
 
-	// 	customerQueue->increaseFilled();
-	// }
+		prevTemp->next = nextTemp;
+		nextTemp->prev = prevTemp;
+		if (tmp == head)
+		{
+			head = nextTemp;
+		}
+		occupied--;
+		free(tmp);
+		return;
+	}
+
+	void checkHead()
+	{
+		// cout << "head customer here: " << head->name << " " << head->energy << endl;
+		// cout << "head customer next here: " << head->next->name << " " << head->next->energy << endl;
+		// cout << "head customer next->next here: " << head->next->next->name << " " << head->next->next->energy << endl;
+		// cout << "head customer next->next->next->head here: " << head->next->next->next->name << " " << head->next->next->next->energy << endl;
+		customer *tmp = head->next;
+	}
 };
